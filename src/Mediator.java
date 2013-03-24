b5d9916d1af2efa43839833e51ce88a26b18d69f
georgiana.scarlat4@gmail.com <mississippi_shou>
2013-03-24 12:57:40 +0200

    Start project

---
+++ src/Mediator.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: nogai
+ * Date: 3/24/13
+ * Time: 12:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Mediator {
+}
